0dbeddb99b35a1e16f974dd268a4b507c46af125
gongbin <blogcool@gmail.com>
2012-04-22 08:01:32 +0800

    test a change

--- mytest81/ViewController.m
+++ mytest81/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSString *abc=@"abc";
+    NSLog(@"abc:%@",abc);
 }
 
 - (void)viewDidUnload
